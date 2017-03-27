1548beff38b4c4c284c9a2bb105fac7aea90aa84
jacekwilczynski <meriin.imw@gmail.com>
2017-03-27 12:08:12 +0200

    Initial commit

---
+++ src/exercise/People.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercise;
+
+import exercise.Person.Gender;
+import java.time.LocalDate;
+import java.time.Year;
+import java.util.Collection;
+import java.util.HashSet;
+
+/**
+ *
+ * @author Jacek
+ */
+public class People {
+
+    Collection<Person> people = new HashSet<>();
+
+    ;
+    
+    public People() {
+
+    }
+
+    public People(int numberToGenerate) {
+        populate(numberToGenerate);
+    }
+
+    public final void populate(int numberToGenerate) {
+        for (int i = 0; i < numberToGenerate; i++) {
+            boolean added;
+            do {
+                Gender gender = randomGender();
+                String name = randomName(gender);
+                LocalDate birthday = randomDate();
+                String emailAddress = generateEmail(name);
+                added = people.add(new Person(name, birthday, gender, emailAddress));
+            } while (!added);
+        }
+    }
+
+    public void print() {
+        System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        for (Person person : people) {
+            builder.append(person).append("\n");
+        }
+        return builder.toString();
+    }
+
+    private int randomInt(int min, int max) {
+        int range = max - min + 1;
+        return (int) (Math.random() * range) + min;
+    }
+
+    private String pickRandom(String[] array) {
+        return array[randomInt(0, array.length - 1)];
+    }
+
+    private String randomName(Gender gender) {
+        final String[][] firstNames = {
+            {"Michael", "George", "Jules", "Arnold", "Terrence", "Louis", "Samuel", "Nate", "Zak", "Donald", "Clint"},
+            {"Amanda", "Jennifer", "Susan", "Nancy", "Deborah", "Theresa", "Barbara", "Hillary", "Patricia", "Melanie"}
+        };
+        final String[] lastNames = {"Jordan", "Clinton", "Trumpet", "Eastwood", "Smith", "Walker", "Hamilton", "Fry", "Mason"};
+
+        int genderInt = gender.ordinal();
+        String firstName = pickRandom(firstNames[genderInt]);
+        String lastName = pickRandom(lastNames);
+        return firstName + " " + lastName;
+    }
+
+    private int numberOfDays(int month, int year) {
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                return 31;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                return 30;
+            case 2:
+                return Year.isLeap(year) ? 29 : 28;
+            default:
+                return 0;
+        }
+    }
+
+    private LocalDate randomDate() {
+        int year = randomInt(1916, 2016);
+        int month = randomInt(1, 12);
+        int day = randomInt(1, numberOfDays(month, year));
+        return LocalDate.of(year, month, day);
+    }
+
+    private Gender randomGender() {
+        return randomInt(0, 1) == 0 ? Gender.MALE : Gender.FEMALE;
+    }
+
+    private String generateEmail(String name) {
+        name = name.replaceAll("[\" \"]", "").toLowerCase();
+        return name + "@provider.com";
+    }
+
+}
---
+++ src/exercise/Person.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercise;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+/**
+ *
+ * @author Jacek
+ */
+public class Person {
+
+    private String name;
+    private LocalDate birthday;
+    private Gender gender;
+    private String emailAddress;
+
+    public Person(String name, LocalDate birthday, Gender gender, String emailAddress) {
+        this.name = name;
+        this.birthday = birthday;
+        this.gender = gender;
+        this.emailAddress = emailAddress;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+    
+    public int getAge() {
+        return birthday.until(LocalDate.now()).getYears();
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public enum Gender {
+        MALE, FEMALE
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 53 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Person other = (Person) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%30s (%s)   %s   %s", name, gender.toString().charAt(0), birthday, emailAddress);
+    }
+    
+    
+}
