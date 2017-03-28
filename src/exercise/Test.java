6fffa06590dd39bf28f98d5848840368b5b00833
jacekwilczynski <meriin.imw@gmail.com>
2017-03-28 14:37:06 +0200

    Prints all females using just the forEach method of the Iterable interface.

--- src/exercise/Test.java
+++ src/exercise/Test.java
@@ -6,17 +6,13 @@ public class Test {
 
     public static void main(String[] args) {
 
-        People people = new People(40);
+        People people = new People(20);
         
-        // Print e-mails of men between 20 and 35
-        people.people
-                .stream()
-                .filter(
-                    p -> p.getGender() == Gender.MALE &&
-                            p.getAge() >= 20 &&
-                            p.getAge() <= 35)
-                .map(p -> p.getEmailAddress())
-                .forEach(email -> System.out.println(email));
+        people.forEach(p -> {
+            if (p.getGender() == Gender.FEMALE) {
+                p.print();
+            }
+        });
         
     }
 
